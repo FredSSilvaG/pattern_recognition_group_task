bd73411d18b7c9cb9650d72205ce026f10083106
mmj12138 <aprilm12138@163.com>
2025-04-08 18:27:35 +0200

    [feat]:add mlp and report

---
+++ mlp/file_utils.py
@@ -0,0 +1,40 @@
+import os
+import pandas as pd
+from PIL import Image
+import numpy as np
+
+def load_image_and_label(image_path, label):
+    image = Image.open(image_path).convert('L')
+    image = image.resize((28, 28))
+    image = np.array(image) / 255.0
+    return image, label
+
+def create_dataset(tsv_path, parent_path, num_samples=None):
+    # load file
+    df = pd.read_csv(tsv_path, sep='\t', header=None)
+    df.columns = ['path', 'label']
+
+    # If the number of samples is specified, the num_samples bar data is taken at random
+    if num_samples is not None:
+        df = df.sample(n=num_samples)
+
+    # get image path and label
+    image_paths = [os.path.join(parent_path, path) for path in df['path']]
+    labels = df['label'].values
+
+    # loading image
+    images = []
+    for path, label in zip(image_paths, labels):
+        image, label = load_image_and_label(path, label)
+        images.append(image)
+
+    images = np.array(images)
+    labels = np.array(labels)
+
+    return images, labels
+
+def get_dataset(parent_path, file_path, num_samples=None):
+
+    X, y = create_dataset(file_path, parent_path, num_samples)
+    X = X.reshape(X.shape[0], -1)
+    return X, y
